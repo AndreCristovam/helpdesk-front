15be67ca8e3782060c8d609cf8917e351704f79f
andre cristovam <andrecristovam@gmail.com>
2022-01-27 00:01:41 -0300

    Tabela Tecnicos criada

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnicos';
+
 
 @Component({
   selector: 'app-tecnico-list',
@@ -6,10 +10,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tecnico-list.component.css']
 })
 export class TecnicoListComponent implements OnInit {
+  
+  ELEMENT_DATA: Tecnico[] =[
+    {
+      id: 1,
+      nome: 'Tomas Shelby',
+      cpf: '123.456.789-10',
+      email: 'tomas@gamil.com',
+      senha: '123',
+      perfis: ['0'],
+      dataCriacao: '15/08/2022'
+    }
+  ]
 
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
