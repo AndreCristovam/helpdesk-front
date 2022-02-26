87b344f7ba42fdc952aefb4c1bb43b54c8c1c692
andre cristovam <andrecristovam@gmail.com>
2022-02-26 16:40:26 -0300

    Apontamento Heroku

--- src/app/config/api.config.ts
+++ src/app/config/api.config.ts
@@ -1,5 +1,5 @@
 export const API_CONFIG = {
-  // baseUrl: 'https://helpdesk-backend-prod.herokuapp.com/'
-  baseUrl: 'http://localhost:8080/'
+  baseUrl: 'https://helpdesk-backend-prod.herokuapp.com/'
+  //baseUrl: 'http://localhost:8080/'
   // baseUrl: "/HEROKU_PROXY",
 };
